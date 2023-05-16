211ab7aa7ef2e13d2a3bad2b5a14d05c353becd3
Yoner Silva <yonerarbeysl@ufps.edu.co>
2023-05-15 22:51:32 -0500

    Cambios al servidor

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:11.0.11
 ARG JAR_FILE=target/UFPSaberPRO-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} .
-CMD [ "java", "-jar",  "/UFPSaberPRO-0.0.1-SNAPSHOT.jar"]
+CMD [ "java", "-jar",  "/UFPSaberPRO-0.0.1-SNAPSHOT.jar","/bin/bash"]
--- src/main/java/com/ufps/UFPSaberPRO/UfpSaberProApplication.java
+++ src/main/java/com/ufps/UFPSaberPRO/UfpSaberProApplication.java
@@ -18,7 +18,7 @@ public class UfpSaberProApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://ec2-18-116-31-71.us-east-2.compute.amazonaws.com:3000");
+				registry.addMapping("/**").allowedOrigins("http://54.235.58.82:3000");
 			}
 		};
 	}
