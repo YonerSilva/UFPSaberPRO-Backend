6e62909e4cdf4d050c239e7ab4d481c741f26860
Yoner Silva <yonerarbeysl@ufps.edu.co>
2023-05-16 18:10:51 -0500

    cambios de servidor ohio

--- src/main/java/com/ufps/UFPSaberPRO/UfpSaberProApplication.java
+++ src/main/java/com/ufps/UFPSaberPRO/UfpSaberProApplication.java
@@ -18,7 +18,7 @@ public class UfpSaberProApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://54.163.51.167:3000");
+				registry.addMapping("/**").allowedOrigins("http://18.223.156.207:3000");
 			}
 		};
 	}
