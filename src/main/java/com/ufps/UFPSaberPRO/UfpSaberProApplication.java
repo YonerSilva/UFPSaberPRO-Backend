0c4afbfec2010acaf3c9ffef1875faf25ba2032d
Yoner Silva <yonerarbeysl@ufps.edu.co>
2023-05-18 20:44:08 -0500

    cambios server2

--- src/main/java/com/ufps/UFPSaberPRO/UfpSaberProApplication.java
+++ src/main/java/com/ufps/UFPSaberPRO/UfpSaberProApplication.java
@@ -18,7 +18,7 @@ public class UfpSaberProApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://3.144.183.144:3000");
+				registry.addMapping("/**").allowedOrigins("http://18.224.139.60:3000");
 			}
 		};
 	}
