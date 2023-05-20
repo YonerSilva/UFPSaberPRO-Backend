ba6353ce0ca575964f22c35980b7ea0265b7d583
Yoner Silva <yonerarbeysl@ufps.edu.co>
2023-05-20 02:37:49 -0500

    Correcion dao simu usu

--- src/main/java/com/ufps/UFPSaberPRO/dao/Simu_UsuRepository.java
+++ src/main/java/com/ufps/UFPSaberPRO/dao/Simu_UsuRepository.java
@@ -15,8 +15,8 @@ public interface Simu_UsuRepository extends CrudRepository<Simu_Usu, Long>{
 	
 	@Transactional
 	@Modifying
-	@Query(value = "SELECT new com.ufps.UFPSaberPRO.dto.UsuarioDTO(u.id_usuario,u.usu_nombre,u.usu_apellido,\r\n"
-			+ "		 u.usu_codigo, u.usu_email,u.programa.id_programa, u.rol.id_rol) FROM Simu_Usu su \r\n"
+	@Query(value = "SELECT new com.ufps.UFPSaberPRO.security.dto.UsuarioDTO(u.id_usuario,u.usu_nombre,u.usu_apellido,\r\n"
+			+ "		 u.usu_codigo, u.usu_email,u.programa.prg_codigo, u.rol.id_rol) FROM Simu_Usu su \r\n"
 			+ "INNER JOIN Usuario u on u.id_usuario = su.usuario.id_usuario \r\n"
 			+ "WHERE su.simulacro.id_simulacro=:id_simulacro")
 	public List<UsuarioDTO> getUsuariosSimu(@Param("id_simulacro") Long simulacro);
